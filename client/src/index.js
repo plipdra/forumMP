df9294925d9b27a27e8854998263768f4513f43f
plipdra <philippe_nikos_edralin@dlsu.edu.ph>
2023-07-19 21:01:45 +0800

    navbar works

--- client/src/index.js
+++ client/src/index.js
@@ -17,12 +17,11 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from 'redux-persist/integration/react';
-import persistReducer from 'redux-persist/es/persistReducer';
 
 const persistConfig = { key: "root", storage, version: 1 };
-const persistReducer = persistReducer(persistConfig, authReducer);
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
-  reducer: persistReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
   getDefaultMiddleware({
     serializableCheck: {
